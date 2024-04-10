c513bf74d254c2e1cea646ce400738a160878283
Attila <b.a.ballaattila@gmail.com>
2024-04-10 17:20:43 +0200

    1. feladat a villogógomb kész

--- Lesson3/VillogoGomb/Form1.cs
+++ Lesson3/VillogoGomb/Form1.cs
@@ -6,5 +6,26 @@ namespace VillogoGomb
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    VillogóGomb b = new VillogóGomb();
+                    b.Width = 40;
+                    b.Height = 40;
+                    b.Left = i * 40;
+                    b.Top = j * 40;
+                    b.Text = (i * j).ToString();
+
+                    Controls.Add(b);
+                }
+            }
+            
+
+            
+        }
     }
 }
---
+++ Lesson3/VillogoGomb/VillogóGomb.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VillogoGomb
+{
+    internal class VillogóGomb : Button
+    {
+        public VillogóGomb()
+        {
+            BackColor = Color.Fuchsia;
+            MouseEnter += VillogoGomb_MouseEnter;
+            MouseLeave += VillogoGomb_MouseLeave;
+        }
+        private void VillogoGomb_MouseLeave(object sender, EventArgs e)
+        {
+            BackColor = SystemColors.ButtonFace;
+        }
+
+        private void VillogoGomb_MouseEnter(object sender, EventArgs e)
+        {
+            BackColor = Color.Yellow;
+        }
+    }
+}
