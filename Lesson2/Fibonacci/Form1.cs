bdc69c51f6b3cac1452e3deb0584d1fd8130a3b8
Attila <b.a.ballaattila@gmail.com>
2024-04-10 16:21:38 +0200

    Fibonacci kesz

--- Lesson2/Fibonacci/Form1.cs
+++ Lesson2/Fibonacci/Form1.cs
@@ -6,5 +6,30 @@ namespace Fibonacci
         {
             InitializeComponent();
         }
+
+        int Fibonacci(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                Button b = new Button();
+                b.Top = i * 30;
+                b.Text = Fibonacci(i).ToString();
+                Controls.Add(b);
+            }
+
+            List<Elem> elemek = new List<Elem>();
+            for (int i = 0; i < 10; i++)
+            {
+                elemek.Add(new Elem { Sorszam = i, Ertek = Fibonacci(i) });
+            }
+            dataGridView1.DataSource = elemek;
+        }
     }
 }
